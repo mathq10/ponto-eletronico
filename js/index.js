a171767ffe2202e8efb2edcf3b72a3e82fe41cfe
Matheus Queirós de Sousa Araújo Carvalho <matheus.queiros@dinfor.uniceub>
2024-09-16 10:41:33 -0300

    BROOOOOOOO

--- js/index.js
+++ js/index.js
@@ -1,45 +1,143 @@
 const diaSemana = document.getElementById("dia-semana");
-const diaMesAno = document.getElementById("dia-mes-ano");
-const horaMinSeg = document.getElementById("hora-min-seg");
-const arrayDayWeek = ["Domingo","Segunda-feira","Terça-feira","Quarta-feira","Quinta-feira","Sexta-feira","Sabado"]
+const dataAtual = document.getElementById("data-atual");
+const horaAtual = document.getElementById("hora-atual");
+const btnRegistrarPonto = document.getElementById("btn-registrar-ponto");
+
+btnRegistrarPonto.addEventListener("click", register);
+
+diaSemana.textContent = getWeekDay();
+dataAtual.textContent = getCurrentDate();
 
 
 const dialogPonto = document.getElementById("dialog-ponto");
 
-const btnRegistrarPonto = document.getElementById("btn-registrar-ponto");
-btnRegistrarPonto.addEventListener("click", () => {
-    dialogPonto.showModal();
+const dialogData = document.getElementById("dialog-data");
+dialogData.textContent = getCurrentDate();
+
+const dialogHora = document.getElementById("dialog-hora");
+dialogHora.textContent = getCurrentTime();
+
+
+const btnDialogEntrada = document.getElementById("btn-dialog-entrada");
+btnDialogEntrada.addEventListener("click", () => {
+    saveRegisterLocalStorage(getObjectRegister("entrada"));
 });
 
 
-const btnDialogFechar = document.getElementById("btn-dialog-fechar");
+const btnDialogSaida = document.getElementById("btn-dialog-saida");
+btnDialogSaida.addEventListener("click", () => {
+    saveRegisterLocalStorage(getObjectRegister("saida"));
+});
+
+
+function getObjectRegister(registerType) {
+    
+    ponto = {
+        "date": getCurrentDate(),
+        "time": getCurrentTime(),
+        "location": getUserLocation(),
+        "id": 1,
+        "type": registerType
+    }
+    return ponto;
+}
+
+const btnDialogFechar = document.getElementById("dialog-fechar");
 btnDialogFechar.addEventListener("click", () => {
     dialogPonto.close();
-});
+})
+
+let registersLocalStorage = getRegisterLocalStorage("register");
+
+function saveRegisterLocalStorage(register) {
+
+    registersLocalStorage.push(register);
+    localStorage.setItem("register", JSON.stringify(registersLocalStorage));
+}
+
+function getRegisterLocalStorage(key) {
+
+    let registers = localStorage.getItem(key);
+
+    if(!registers) {
+        return [];
+    }
+
+    return JSON.parse(registers);
+}
+
+// O que é uma função assíncrona?
+// O que é um objeto Javascript?
+// O que é uma instância?
+// O que é PROTOTYPE?
+function getUserLocation() {
+    navigator.geolocation.getCurrentPosition((position) => {   
+        let userLocation = {
+            "lat": position.coords.latitude,
+            "long": position.coords.longitude
+        }
+        return userLocation;
+    });
+}
 
 
 
-function daySemana() {
-    //retornar dia da semana
+// Como garantir que uma função assíncrona já foi executada/processada?
+// Possíveis soluções
+
+//getUserLocation(functionCallback) {
+    //navigator.geolocation.getCurrentPosition((position) => {
+        //userLocation = {
+            //OBJETO com lat e long
+        //}
+        //functionCallback(userLocation)
+    //})
+//}
+
+// OU
+
+//getUserLocation() {
+    //return new Promise((suc, fail) => {
+        //navigator.geolocation.getCurrentPosition()
+    //})
+
+    
+//}
+ 
+
+
+function register() {
+    dialogPonto.showModal();
+}
+
+
+function updateContentHour() {
+    horaAtual.textContent = getCurrentTime();
+}
+
+// Retorna a hora atual (hora/minuto/segundo)
+function getCurrentTime() {
     const date = new Date();
-    return arrayDayWeek[date.getDay()];
+    return String(date.getHours()).padStart(2, '0') + ":" + String(date.getMinutes()).padStart(2, '0') + ":" + String(date.getSeconds()).padStart(2, '0');
 }
 
-function dataCompleta() {
-    const date = new Date();
-    return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+// Retorna a data atual no padrão dd/mm/aaaa
+function getCurrentDate() {
+    const date = new Date(); 
+    let mes = date.getMonth() + 1;
+    return String(date.getDate()).padStart(2, '0') + "/" + String(mes).padStart(2, '0') + "/" +  String(date.getFullYear()).padStart(2, '0');
 }
 
-function horaCompleta() {
-    const date = new Date();
-    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+function getWeekDay() {
+    const date = new Date()
+    const day = date.getDay()
+    const daynames = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+    return daynames[day]
 }
 
-function atualizaHora() {
-    horaMinSeg.textContent = horaCompleta();
-}
+updateContentHour();
+setInterval(updateContentHour, 1000);
 
-setInterval(atualizaHora, 1000);
-
-diaSemana.textContent = daySemana();
-diaMesAno.textContent = dataCompleta();
+console.log(getCurrentTime());
+console.log(getCurrentDate());
+console.log(getWeekDay());
